728a6b1052ff43ed40ed99915694e1053c31f581
SocialSupaCrew <SocialSupaCrew@gmail.com>
2013-02-04 09:47:18 +0100

    Ajout classe parametes

--- src/Settings/Parametres.java
+++ src/Settings/Parametres.java
@@ -1,13 +1,37 @@
 package Settings;
 
 public class Parametres {
+	
+	private int gridSize[] = new int[2];
+	private int nbPas;
+	private float waitTime;
+	
+	public int[] getGridSize() {
+		return gridSize;
+	}
 
-	/**
-	 * @param args
-	 */
+	public void setGridSize(int[] gridSize) {
+		this.gridSize = gridSize;
+	}
+
+	public int getNbPas() {
+		return nbPas;
+	}
+
+	public void setNbPas(int nbPas) {
+		this.nbPas = nbPas;
+	}
+
+	public float getWaitTime() {
+		return waitTime;
+	}
+
+	public void setWaitTime(float waitTime) {
+		this.waitTime = waitTime;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
