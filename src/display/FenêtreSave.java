1081d3ce947ad9379270ec38c6e70ab0d675412d
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2013-02-04 13:28:37 +0100

    Ajout bouton quitter
    Signed-off-by: Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>

--- src/display/FenêtreSave.java
+++ src/display/FenêtreSave.java
@@ -45,7 +45,7 @@ public class FenêtreSave {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 354, 209);
+		frame.setBounds(100, 100, 354, 214);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
 				ColumnSpec.decode("30px"),
@@ -56,7 +56,9 @@ public class FenêtreSave {
 				RowSpec.decode("30px"),
 				RowSpec.decode("23px"),
 				RowSpec.decode("65px"),
-				RowSpec.decode("23px"),}));
+				RowSpec.decode("23px"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
 		
 		JComboBox comboBox = new JComboBox();
 		frame.getContentPane().add(comboBox, "2, 2, fill, center");
@@ -68,7 +70,10 @@ public class FenêtreSave {
 		frame.getContentPane().add(btnNewButton, "2, 4, left, center");
 		
 		JButton btnImporter = new JButton("Importer");
-		frame.getContentPane().add(btnImporter, "4, 4, center, center");
+		frame.getContentPane().add(btnImporter, "4, 4, fill, center");
+		
+		JButton btnQuitter = new JButton("Quitter");
+		frame.getContentPane().add(btnQuitter, "4, 6, fill, default");
 	}
 
 }
